6e88eb7539fe265432b93550c56d4a5060111104
Oskar Pol <oskarbank@gmail.com>
2017-03-18 15:17:54 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by OskarPraca on 2017-03-18.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("Oskar", "Polak", 30));
+        personList.add(new Person("Adam", "Trybancki", 70));
+        personList.add(new Person("Karol", "Nowak", 54));
+        personList.add(new Person("Damian", "Góra", 93));
+        personList.add(new Person("Wiktor", "Nowicki", 23));
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Aasda");
+        stringList.add("Tomek");
+        stringList.add("Ada");
+        stringList.add("Filemon");
+
+        //Collections.sort(personList);
+
+        for(Person person : personList) {
+           System.out.println(person.getName());
+        }
+
+
+
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,39 @@
+/**
+ * Created by OskarPraca on 2017-03-18.
+ */
+public class Person {
+
+    private String name;
+    private String lastname;
+    private int age;
+
+    public Person(String name, String lastname, int age) {
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
